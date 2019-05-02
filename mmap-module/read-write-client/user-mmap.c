ecb0f61aa05fdb38c46c3d2bc38645eb177b42f4
Amirhossein <amirsorouri26@gmail.com>
2019-05-02 16:27:48 +0430

    some cleaning stuff to befor starting the implementation of benchmarking with other mechanisms(splice, vmsplice, ...).

--- mmap-module/read-write-client/user-mmap.c
+++ mmap-module/read-write-client/user-mmap.c
@@ -17,7 +17,7 @@ enum { BUFFER_SIZE = 4 };
 
 int main(int argc, char **argv)
 {
-	int fd, fd2;
+	int fd;
 	long page_size;
 	char *address1, *address2;
 	char buf[BUFFER_SIZE];
@@ -30,43 +30,35 @@ int main(int argc, char **argv)
 	page_size = sysconf(_SC_PAGE_SIZE);
 	printf("open pathname = %s\n", argv[1]);
 	fd = open(argv[1], O_RDWR | O_SYNC);
-	fd2 = open("../../random-files/20m-files/file.txt", O_RDONLY);
-	if (fd < 0) {
+	int data_to_write_fd = open("../../random-files/20m-files/file.txt", O_RDONLY);
+	if (fd < 0 || data_to_write_fd < 0) {
 		perror("open");
 		assert(0);
 	}
 	printf("fd = %d\n", fd);
+	printf("data_to_write_fd = %d\n", data_to_write_fd);
 
-    /* mmap twice for double fun. */
+    /* mmap the file */
 	puts("mmap 1");
 	address1 = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	if (address1 == MAP_FAILED) {
 		perror("mmap");
 		assert(0);
 	}
-	puts("mmap 2");
-	address2 = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (address2 == MAP_FAILED) {
-		perror("mmap");
-		return EXIT_FAILURE;
-	}
-	assert(address1 != address2);
 
 	/* Check that the physical addresses are the same.
 	 * They are, but TODO why virt_to_phys on kernel gives a different value? */
 	assert(!virt_to_phys_user(&paddr, getpid(), (uintptr_t)address1));
 	printf("paddr1 = 0x%jx\n", (uintmax_t)paddr);
-	assert(!virt_to_phys_user(&paddr, getpid(), (uintptr_t)address2));
-	printf("paddr2 = 0x%jx\n", (uintmax_t)paddr);
 
 	int cnt = 0;
 	clock_t start_t, end_t, total_t;
+	printf("Starting of the program, start_t = %ld\n", start_t);
 	start_t = clock();
-	
-	
-	while(0 != read(fd2, buf, BUFFER_SIZE)){
+
+	while(0 != read(data_to_write_fd, buf, BUFFER_SIZE))
 		strcpy(address1, buf);
-	}
+
 	end_t = clock();
 	printf("End of the big loop, end_t = %ld\n", end_t);
 	
@@ -76,11 +68,6 @@ int main(int argc, char **argv)
     /* Cleanup. */
     puts("munmap 1");
 	if (munmap(address1, page_size)) {
-		perror("munmap");
-		assert(0);
-	}
-    puts("munmap 2");
-	if (munmap(address2, page_size)) {
 		perror("munmap");
 		assert(0);
 	}
